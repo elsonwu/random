6aee14720d5d2e89ca77f8511cd01d801d10d92b
elsonwu <elsonwu@126.com>
2014-05-17 11:04:56 +0800

    improve the String method performance

--- rand.go
+++ rand.go
@@ -1,7 +1,10 @@
 package random
 
 import (
+	"crypto/md5"
+	"fmt"
 	"math/rand"
+	"strconv"
 	"time"
 )
 
@@ -17,6 +20,12 @@ const (
 	CHAR_LEN         = len(CHAR)
 )
 
+func Md5(str string) string {
+	h := md5.New()
+	h.Write([]byte(str))
+	return fmt.Sprintf("%x", h.Sum(nil))
+}
+
 func Int(min, max int) int {
 	source := rand.NewSource(time.Now().UnixNano())
 	return min + rand.New(source).Intn(max-min)
@@ -28,7 +37,27 @@ func Char() string {
 }
 
 func String(length int) string {
-	return strs(Char, length)
+	s := ""
+	if length > 32 {
+		var ts int = length / 32
+		for i := 0; i < ts; i++ {
+			s += String(32)
+		}
+
+		if tr := length % 32; tr > 0 {
+			s += String(tr)
+		}
+
+		return s
+	}
+
+	s = Md5(strconv.Itoa(time.Now().Nanosecond()))
+	if length == 32 {
+		return s
+	}
+
+	return s[0:length]
+
 }
 
 func Letter() string {
